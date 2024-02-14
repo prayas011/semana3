8369168ede7306d3a817010624273f3cf3ed5c57
prayas011 <68487783+prayas011@users.noreply.github.com>
2024-02-14 11:44:32 -0600

    Primer commit y final

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function showform1(){
+var form1=document.getElementById("column1");
+var form2=document.getElementById("column2");
+form1.style.display="block";
+form2.style.display="none";
+}
+
+function showform2(){
+    var form1=document.getElementById("column1");
+    var form2=document.getElementById("column2");
+    form1.style.display="none";
+    form2.style.display="block";
+    }
